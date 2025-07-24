42933558edde23610a2dfb6ed6ee26323d70aca4
MyCadence <arronbirhanu03@gmail.com>
2025-07-24 01:01:53 -0400

    Setup DRF authentication and permissions

--- FullStackProjects/BudgetingApplication/finance_manager/expenses/urls.py
+++ FullStackProjects/BudgetingApplication/finance_manager/expenses/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ExpenseListCreate, ExpenseDetail
+from .views import ExpenseListCreate, ExpenseDetail, welcome
 
 urlpatterns = [
+    path('', welcome, name='welcome'),
     path('expenses/', ExpenseListCreate.as_view(), name='expense-list-create'),
     path('expenses/<int:pk>/', ExpenseDetail.as_view(), name='expense-detail'),  # New URL for single expense
 ]
--- FullStackProjects/BudgetingApplication/finance_manager/expenses/views.py
+++ FullStackProjects/BudgetingApplication/finance_manager/expenses/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from .models import Expense
 from .serializers import ExpenseSerializer
+from django.http import HttpResponse
+
+def welcome(request):
+    return HttpResponse("Welcome to the Budgeting API. Use 'Your Current URL'/api/expenses/ to access expenses.")
 
 class ExpenseListCreate(generics.ListCreateAPIView):
     queryset = Expense.objects.all()
